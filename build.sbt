58becbcab4d1fba72e6e1708badf0f3997321da8
Taylor Bird <nsubordin81@gmail.com>
2022-01-25 22:52:00 -0500

    adding the scalajs dom library and then using the dom to add a paragraph node

--- build.sbt
+++ build.sbt
@@ -4,3 +4,6 @@ name := "Scala.js Blog"
 scalaVersion := "3.0.2"
 
 scalaJSUseMainModuleInitializer := true
+libraryDependencies += ("org.scala-js" %%% "scalajs-dom" % "1.1.0").cross(
+  CrossVersion.for3Use2_13
+)
--- src/main/scala/tutorial/webapp/TutorialApp.scala.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala.scala
@@ -1,7 +1,13 @@
 package tutorial.webapp
 
-object TutorialApp {
-  def main(args: Array[String]): Unit = {
-      println("Hello world!")
-  }
-}
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+def appendPar(targetNode: dom.Node, text: String): Unit =
+  val parNode = document.createElement("p")
+  parNode.textContent = text
+  targetNode.appendChild(parNode)
+
+object TutorialApp:
+  def main(args: Array[String]): Unit =
+    appendPar(document.body, "Hello Taylor")
