18131e19c0548175896cc8a4bcc98af04a7a1770
Taylor Bird <nsubordin81@gmail.com>
2022-01-26 20:10:11 -0500

    adding user interaction and DOM setup in code

--- src/main/scala/tutorial/webapp/TutorialApp.scala.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala.scala
@@ -2,12 +2,26 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 def appendPar(targetNode: dom.Node, text: String): Unit =
   val parNode = document.createElement("p")
   parNode.textContent = text
   targetNode.appendChild(parNode)
 
+def addClickedMessage(): Unit =
+  appendPar(document.body, "You clicked the button!")
+
 object TutorialApp:
   def main(args: Array[String]): Unit =
-    appendPar(document.body, "Hello Taylor")
+    document.addEventListener("DOMContentLoaded", (e: dom.Event) => setupUI())
+
+def setupUI(): Unit =
+  val button = document.createElement("button")
+  button.textContent = "Click me!"
+  button.addEventListener(
+    "click",
+    (e: dom.MouseEvent) => addClickedMessage()
+  )
+  document.body.appendChild(button)
+  appendPar(document.body, "Hello Taylor")
