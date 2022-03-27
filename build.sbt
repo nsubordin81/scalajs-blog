957b0d49986ce3534ffdf68df9ff5d79fa1d1f9e
Taylor Bird <nsubordin81@gmail.com>
2022-03-27 15:41:57 -0400

    Laminar gets me to a hello world, so now I can make my spa app.

--- build.sbt
+++ build.sbt
@@ -2,11 +2,12 @@ val scala3Version = "3.1.1"
 
 lazy val root = project
   .in(file("."))
+  .enablePlugins(ScalaJSPlugin)
   .settings(
     name := "scalajs-blog",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    scalaJSUseMainModuleInitializer := true,
+    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
+    libraryDependencies += "com.raquo" %%% "laminar" % "0.13.1"
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,39 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import org.scalajs.dom
+import com.raquo.laminar.api.L.{*, given}
 
-def msg = "I was compiled by Scala 3. :)"
+// val helloDiv: Div = dev("Hello, World!")
+
+// @main def hello: Unit =
+//   println("Hello world!")
+//   println(msg)
+
+// def msg = "I was compiled by Scala 3. :)"
+object ScalaJSBlog {
+  def main(args: Array[String]): Unit =
+    println("Hello world!")
+    documentEvents.onDomContentLoaded.foreach { _ =>
+      val appContainer = dom.document.querySelector("#appContainer")
+      val appElement = div(h1("Hello world"))
+      render(appContainer, appElement)
+    }(unsafeWindowOwner)
+}
+
+// val nameVar = Var(initial = "world")
+
+// val rootElement = div(
+//   label("Your name: "),
+//   input(
+//     onMountFocus,
+//     placeholder := "Enter your name here",
+//     inContext { thisNode => onInput.map(_ => thisNode.ref.value) --> nameVar }
+//   ),
+//   span(
+//     "Hello, ",
+//     child.text <-- nameVar.signal.map(_.toUpperCase)
+//   )
+// )
+
+// // In most other examples, containerNode will be set to this behind the scenes
+// val containerNode = dom.document.querySelector("#mdoc-html-run0")
+
+// render(containerNode, rootElement)
