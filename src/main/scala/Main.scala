08e17a822e69b2485e3bed60f29e33a3b1886a5a
Taylor Bird <nsubordin81@gmail.com>
2022-03-30 15:41:24 -0400

    [feat] set up the basic structure of a page

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,11 +10,21 @@ import com.raquo.laminar.api.L.{*, given}
 // def msg = "I was compiled by Scala 3. :)"
 object ScalaJSBlog {
   def main(args: Array[String]): Unit =
-    println("Hello world!")
     documentEvents.onDomContentLoaded.foreach { _ =>
       val appContainer = dom.document.querySelector("#appContainer")
-      val appElement = div(h1("Hello world"))
-      render(appContainer, appElement)
+      val head = header(h1("Synapse Flux"))
+      val navigation = nav(
+        button("bio"),
+        button("music"),
+        button("art"),
+        button("programming"),
+        button("the space behind")
+      )
+      // main(section("this is some text")),
+      val foot = footer("author Taylor Bird")
+      render(appContainer, head)
+      render(appContainer, navigation)
+      render(appContainer, foot)
     }(unsafeWindowOwner)
 }
 
